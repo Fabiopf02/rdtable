f460b6883b205dad6d8c75b502a969d60d186e03
fabiofranca02 <fabio.franca@algoritme.com.br>
2022-05-16 14:19:09 -0300

    configuração eslint/prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,47 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
-  extends: ['plugin:react/recommended', 'eslint:recommended', 'plugin:react/recommended', 'standard', 'plugin:prettier/recommended', 'plugin:storybook/recommended'],
+  extends: [
+    'plugin:react/recommended',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'standard',
+    'plugin:prettier/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
   },
   settings: {
     react: {
-      version: 'detect'
-    }
+      version: 'detect',
+    },
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
     'react/react-in-jsx-scope': 'off',
-    'prettier/prettier': ['error', {
-      singleQuote: true
-    }]
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        endOfLine: 'auto',
+      },
+    ],
   },
-  overrides: [{
-    files: ['**/*.js', '**/*.jsx'],
-    rules: {
-      '@typescript-eslint/no-var-requires': 'off',
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-      'react/display-name': 'off'
-    }
-  }]
-};
+  overrides: [
+    {
+      files: ['**/*.js', '**/*.jsx'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        'react/display-name': 'off',
+      },
+    },
+  ],
+}
